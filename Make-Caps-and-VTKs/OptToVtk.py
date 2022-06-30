3fd1d20e8181f40116932753a6833e6fa6274bd5
Ömer F. BODUR <ofbodur@gmail.com>
2022-06-30 11:00:57 +1000

    Update

--- Make-Caps-and-VTKs/OptToVtk.py
+++ Make-Caps-and-VTKs/OptToVtk.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2.7 
-# Converted to python 3 on 30 May 2022 (Omer Bodur)
+# Update: Converted to python 3.8 on 30 May 2022 (Ömer F. Bodur), University of Wollongong.
 
 '''
 /*
@@ -20,7 +20,24 @@
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple 
  * Place, Suite 330, Boston, MA 02111-1307 USA
  * ===================================================================================== 
- * Updated by Omer Bodur, University of Wollongong (30/05/2022)
+
+ * ===================================================================================== 
+ * Updated by Ömer F. Bodur at University of Wollongong (30/05/2022).
+
+ * This program is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation; either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple 
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ * ===================================================================================== 
  */
 '''
 
@@ -124,8 +141,6 @@ Usage: ./capToVtk <capfile> <pidfile>
 #
 #     def GetNumberOfPieces(self):
 #         return self.__NumberOfPieces
-
-
 
 
 
